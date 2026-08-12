4aaa34412f24f8c1229ba3fed054fc9845ca2881
gilleece <seangilleece@gmail.com>
2026-08-12 15:21:58 +0100

    Fix startup crash on devices missing the GameManager service

--- app/src/main/java/com/limelight/utils/UiHelper.java
+++ app/src/main/java/com/limelight/utils/UiHelper.java
@@ -32,6 +32,10 @@ public class UiHelper {
     private static void setGameModeStatus(Context context, boolean streaming, boolean interruptible) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             GameManager gameManager = context.getSystemService(GameManager.class);
+            if (gameManager == null) {
+                // Service is missing on some devices, Quest headsets for example
+                return;
+            }
 
             if (streaming) {
                 gameManager.setGameState(new GameState(false, interruptible ? GameState.MODE_GAMEPLAY_INTERRUPTIBLE : GameState.MODE_GAMEPLAY_UNINTERRUPTIBLE));
